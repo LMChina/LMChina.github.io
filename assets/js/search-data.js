96b33e73910baecf00cb85a0f9bde7c8f19f1672
LMChina <49117765+LMChina@users.noreply.github.com>
2026-08-09 00:09:38 +0000

    Deploying to gh-pages from @ LMChina/LMChina.github.io@442a2fbeb17181e471713f12a3d8a30e2e934e06 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,6 +490,13 @@ ninja.data = [{
           window.open("https://github.com/LMChina", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/mao-li-1522b5292", "_blank");
+        },
+      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
